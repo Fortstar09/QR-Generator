b90f3702175282bd0e2ece6939143592bdeec30b
Fortstar09 <ogunyafort@gmail.com>
2024-10-03 23:46:29 +0100

    Complete the customize modal

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let inputData = document.getElementById("data");
+let sizeValue = document.getElementById("size");
+let fgColor = document.getElementById("fgColor");
+let bgColor = document.getElementById("bgColor");
 const generateBtn = document.getElementById("generate");
+const editBtn = document.getElementById("edit");
 let image = document.getElementById("image");
 let anchor = document.getElementById('anchor');
 
 
+
+
+function getEdit() {
+
+}
+
 function buttonClick() {
     let data = inputData.value;
-    var fmt = 'png'
+    let size = sizeValue.value;
+    let fg_color = fgColor.value;
+    let bg_color = bgColor.value;
+
+    console.log(data, size, fg_color, bg_color);
+
     $.ajax({
         method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/qrcode?data=' + data + '&format=' + fmt,
+        url: `https://api.api-ninjas.com/v1/qrcode?data=${data}&format=png&size=${size}&fg_color=${fg_color}&bg_color=${bg_color}`,
         headers: { 'X-Api-Key': 'nqlaCF35mmdeLKIURJdUQQ==uGyl03HDBQFu3QXW' },
         contentType: 'application/json',
         success: function (result) {
@@ -24,6 +39,7 @@ function buttonClick() {
 }
 
 generateBtn.addEventListener("click", buttonClick);
+editBtn.addEventListener("click", buttonClick);
 
 
 // var data = 'https://api-ninjas.com';
